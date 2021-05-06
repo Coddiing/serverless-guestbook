97cf5933c0c39f713f66c6f232c813b9acdb7538
Victory <Victory@ibm.com>
2021-05-06 14:40:17 +0000

    init commit

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://6c01f567.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
